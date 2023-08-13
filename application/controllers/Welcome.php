4bdfd82664136920d3552226ce487eeed7c487df
unknown <arastu.dixit13@gmail.com>
2023-08-14 00:34:51 +0530

    Initial commit

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,18 @@
+
+
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+#[\AllowDynamicProperties]
+class Welcome extends CI_Controller {
+//Validating login
+function __construct(){
+parent::__construct();
+if(!$this->session->userdata('uid'))
+redirect('signin');
+}
+public function index(){
+$userfname=$this->session->userdata('fname');	
+$this->load->view('welcome',['firstname'=>$userfname]);
+}
+}
---
+++ ci3.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 12, 2023 at 09:27 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ci3`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblusers`
+--
+
+CREATE TABLE `tblusers` (
+  `id` int(11) NOT NULL,
+  `FirstName` varchar(150) DEFAULT NULL,
+  `LastName` varchar(150) DEFAULT NULL,
+  `Email` varchar(150) DEFAULT NULL,
+  `Password` varchar(255) DEFAULT NULL,
+  `PostingDate` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblusers`
+--
+
+INSERT INTO `tblusers` (`id`, `FirstName`, `LastName`, `Email`, `Password`, `PostingDate`) VALUES
+(0, 'admin', 'new', 'admin@gmail.com', '123456', '2023-08-07 13:25:50');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
